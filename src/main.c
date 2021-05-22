a009b5ebe785c838252e17b4469b04dfc5990e36
vinilopes91 <vinilopes13@hotmail.com>
2021-05-22 18:31:11 -0300

    wip: implementando sendS e receiveS

--- src/main.c
+++ src/main.c
@@ -1,9 +1,42 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/shm.h>
+#include <semaphore.h>
+#include <stdint.h>
 #include "ipc.h"
+
+void *handle_thread(void *ptr);
 
 int main(void)
 {
-    puts("test");
-    initSM();
+    int smid = initSM();
+    pthread_t thread;
+    ipc_message teste = "abcde";
+
+    pthread_create(&thread, NULL, handle_thread, (void *)(intptr_t)smid);
+
+    sleep(2);
+    sendS(smid, teste);
+
+    pthread_join(thread, NULL);
+
     return 0;
+}
+
+void *handle_thread(void *ptr)
+{
+    int smid = (intptr_t) ptr;
+    ipc_message msg;
+
+    printf("Esperando receber mensagem\n");
+    receiveS(smid, msg);
+    printf("Mensagem recebida: %s\n", (char *)msg);
+
+    pthread_exit(NULL);
 }
