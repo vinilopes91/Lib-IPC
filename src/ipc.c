9e6ee2d22c76ea4cd473095aaa8a68ab6363eb9f
Leonardo Marinho de Melo Júnior <41721777+Leommjr@users.noreply.github.com>
2021-06-11 14:49:21 -0300

    Removendo verificação de escrita

--- src/ipc.c
+++ src/ipc.c
@@ -20,7 +20,7 @@ typedef struct lista
 {
     int qtd;
     message_data buffer[MAX_BUFFER];
-    int write;
+    // int write;
 } lista;
 
 typedef struct lista_threads
@@ -80,7 +80,7 @@ int initSM()
             printf("sem_init mutex_buffer falhou\n");
             return -1;
         }
-	    shared_area_ptr->list.write = 0;
+	    // shared_area_ptr->list.write = 0;
         shared_area_ptr->list.qtd = 0;
 
         threads_registry.threads[threads_registry.qtd].thread_id = pthread_self();
@@ -121,33 +121,33 @@ int sendA(pthread_t dest_id, ipc_message message){
     int msg_index = find_message_index(&shared_area_ptr->list, pthread_self(), dest_id);
     if (msg_index != -1)
     {
-	if(shared_area_ptr->list.write == 0)
-	{
-	        shared_area_ptr->list.write = 1;
-            strcpy((char *)shared_area_ptr->list.buffer[msg_index].message, (char *)message);
-	        shared_area_ptr->list.write = 0;
-	        return 0;
-	}
-	else
-		return -2;//está ocupado
+	//if(shared_area_ptr->list.write == 0)
+	//{
+	//    shared_area_ptr->list.write = 1;
+        strcpy((char *)shared_area_ptr->list.buffer[msg_index].message, (char *)message);
+	    shared_area_ptr->list.write = 0;
+	    return 0;
+	//}
+	//else
+	//	return -2;//está ocupado
     }
     else if(shared_area_ptr->list.qtd < MAX_BUFFER) {
-	if(shared_area_ptr->list.write == 0){
-	        shared_area_ptr->list.write = 1;	
-            if(push(&shared_area_ptr->list, pthread_self(), dest_id, message) == -1){
-	            shared_area_ptr->list.write = 0;
+	//if(shared_area_ptr->list.write == 0){
+	//        shared_area_ptr->list.write = 1;	
+        if(push(&shared_area_ptr->list, pthread_self(), dest_id, message) == -1){
+	    //        shared_area_ptr->list.write = 0;
 		    return -1;
 	    }
 	    else{
-		    shared_area_ptr->list.write = 0;
+		    //shared_area_ptr->list.write = 0;
 		    return 0;
 	    }
-	}
-	else
-		return -2;//está ocupado
+	//}
+	//else
+	//	return -2;//está ocupado
     }
     else {
-	    return -1;
+	    return -1; // cheio
     }
 }
 
